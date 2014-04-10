7d4b6fedb56455682289ccb19e11cc088b45ef3c
jukkahyvarin <jukka.hyvarin@gmail.com>
2014-04-10 16:11:12 +0300

    fixes

--- www/beacon.js
+++ www/beacon.js
@@ -1,10 +1,13 @@
 var beacon = {
-    startRanging: function(successCallback, errorCallback) {
+    startRanging: function(message, successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'BeaconPlugin', // mapped to our native Java class called "Calendar"
             'ranging', // with this action name
+             [{                  // and this array of custom arguments to create our entry
+                 "message": message
+             }]
         ); 
      }
 }
